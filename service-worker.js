888966bbb3a6f219c5567efa64d7761862243fda
owen672 <tecnowebsolutions59@gmail.com>
2025-08-25 12:33:49 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,20 @@
-const CACHE_NAME = "patinaje-v1";
-const URLS_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
-];
-
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+    caches.open("patinaje-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/icons/icon-192.png",
+        "/icons/icon-512.png"
+      ]);
+    })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
